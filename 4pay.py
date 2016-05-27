a0f31ccf4cd5489397c3593c1479b6699f7b4229
imanohar <indu.manohar@gmail.com>
2016-05-27 17:04:28 +0530

    Update 4pay.py

--- 4pay.py
+++ 4pay.py
@@ -1,29 +1,26 @@
-h=raw_input('Input work hours:\n') #I can't seem to write these prompts into the function def itself
+h=raw_input('Input work hours:\n')
 r=raw_input('Input hourly rate:\n')
 
 def computepay(h,r):
-    if float(h) > 40:
-        p=(40*float(r))+((float(h)-40)*float(r)*1.5)
-    elif float(h)<=40:
-        p=float(h)*float(r)
-    print "Here's how much you made:"
-    print p
-    again=raw_input("Would you like to make another calculation? y/n\n")
-    if again=="y":
-        h=raw_input('Input work hours:\n') #See it crops up here again
-        r=raw_input('Input hourly rate:\n')
-        computepay(h,r)
-    else:
-        print "Ok, goodbye."
+    try:
+        if float(h) > 40:
+            p = (40*float(r))+((float(h)-40) * float(r)*1.5) 
+        elif float(h)<=40:
+            p=float(h)*float(r)
+        print "Here's how much you made:"
+        print p
+    except:
+        print "Error, please input valid numerals."
 
-try:
-    computepay(h,r)
-except:
-    print "Error, please input valid numerals."
-    again=raw_input("Would you like to try again?y/n\n")
-    if again=="y":
-        h=raw_input('Input work hours:\n') #And again!
+
+    again = raw_input("Would you like to try again? y/n\n")
+    if str.lower(again) == "y":
+        h=raw_input('Input work hours:\n')
         r=raw_input('Input hourly rate:\n')
         computepay(h,r)
-    else:
+    elif str.lower(again) == "n":
         print "Ok, goodbye."
+    else:
+        print "Huh? Ok bai."
+
+computepay(h,r)
